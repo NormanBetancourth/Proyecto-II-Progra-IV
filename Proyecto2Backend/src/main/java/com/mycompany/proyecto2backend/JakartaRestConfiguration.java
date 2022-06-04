0aebbedc0e126d24c933f7efef29821c9f21e743
NormanBetancourth <norman.betancourtt.mairena@est.una.ac.cr>
2022-06-04 01:03:29 -0600

    Setting up backend and front end

---
+++ Proyecto2Backend/src/main/java/com/mycompany/proyecto2backend/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.mycompany.proyecto2backend;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("api")
+public class JakartaRestConfiguration extends Application {
+    
+}
